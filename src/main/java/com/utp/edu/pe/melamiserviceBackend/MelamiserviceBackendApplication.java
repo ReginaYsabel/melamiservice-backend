57357f7e721bb9a83fb50ac2dfbaf6a35c9db417
ysabel <regina.pujay@gmail.com>
2024-05-19 22:18:15 -0500

    add project folder

---
+++ src/main/java/com/utp/edu/pe/melamiserviceBackend/MelamiserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.utp.edu.pe.melamiserviceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelamiserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelamiserviceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/edu/pe/melamiserviceBackend/MelamiserviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.edu.pe.melamiserviceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelamiserviceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
